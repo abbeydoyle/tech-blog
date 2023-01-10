da79f210dc9cd016e2e02aac85ac4ddc612a34d2
Abigail Doyle <abigaildoyle3@gmail.com>
2023-01-10 11:43:39 -0800

    auth.js util

--- utils/auth.js
+++ utils/auth.js
@@ -1,1 +1,10 @@
-// TODO: create auth util
+const withAuth = (req, res, next) => {
+      // If the user is not logged in, redirect the request to the login route
+      if (!req.session.user_id) {
+        res.redirect('/login');
+      } else {
+        next();
+      }
+};
+    
+module.exports = withAuth;
