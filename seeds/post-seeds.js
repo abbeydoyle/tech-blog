d3b010342215dfae30bc47d593b4800acdbb5962
Abigail Doyle <abigaildoyle3@gmail.com>
2023-01-10 14:23:53 -0800

    seeds.js no iterators

--- seeds/post-seeds.js
+++
@@ -1,1 +0,0 @@
-// TODO: create post seed export
--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,1 +1,38 @@
-// TODO: create megazord seed export
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+      await sequelize.sync({ force: true });
+
+      const users = await User.bulkCreate(userData, {
+            individualHooks: true,
+            returning: true,
+      });
+
+      // TODO: does this need to be an iterator?
+      // for (const project of projectData) {
+      //       await Project.create({
+      //         ...project,
+      //         user_id: users[Math.floor(Math.random() * users.length)].id,
+      //       });
+      // }
+
+      const posts = await Post.bulkCreate(postData, {
+            individualHooks: true,
+            returning: true,
+      });
+
+      const comments = await Comment.bulkCreate(commentData, {
+            individualHooks: true,
+            returning: true,
+      });
+
+      process.exit(0);
+
+}
+
+seedDatabase();
