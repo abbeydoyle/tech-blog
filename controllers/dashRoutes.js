a7e72638d8e7737110ea01fe0ecf4a4ac98416bb
Abigail Doyle <abigaildoyle3@gmail.com>
2023-01-31 18:18:58 -0800

    temp

--- controllers/dashRoutes.js
+++ controllers/dashRoutes.js
@@ -78,37 +78,50 @@ router.get('/edit/:id', withAuth, (req, res) => {
       }
 
       const post = dbPostData.get({ plain: true });
-      res.render('edit-post', { post, loggedIn: true });
+      res.render('editpost', { post, loggedIn: true });
     })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
 });
-  
-// get route user from dashboard, wont return password
-router.get('/edituser', withAuth, (req, res) => {
-  User.findOne({
-    attributes: { exclude: ['password'] },
+
+router.get('/create', withAuth, (req, res) => {
+  Post.findAll({
     where: {
-
-      id: req.session.user_id
-    }
-  })
-    .then(dbUserData => {
-      if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
-        return;
+      // use the ID from the session
+      user_id: req.session.user_id
+    },
+    attributes: [
+      'id',
+      'title',
+      'created_at',
+      'post_body'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_body', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
       }
-
-      const user = dbUserData.get({ plain: true });
-      res.render('edit-user', {user, loggedIn: true});
+    ]
+  })
+    .then(dbPostData => {
+      // serialize data before passing to template
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('createpost', { posts, loggedIn: true });
     })
     .catch(err => {
-
       console.log(err);
       res.status(500).json(err);
-    })
-  });
+    });
+});
 
 module.exports = router;
